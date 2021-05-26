8342dbb6a549ee2c39ec128051c25533429ca820
Revanth <revanthmopidevi@gmail.com>
2021-05-26 09:28:29 +0530

    admin-model and route

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -6,9 +6,7 @@ router.post('/create', async (req, res) => {
     console.log("0")
     const admin = new Admin(req.body)
     try {
-        console.log("1")
         await admin.save()
-        console.log("2")
         const token = admin.generateAuthToken()
         res.status(201).send({admin, token:token})
     } catch (error) {
