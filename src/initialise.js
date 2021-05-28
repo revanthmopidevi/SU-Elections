652968b8828d4e914b1049a5e7c3358119bda3e3
Revanth Mopidevi <39304828+revanthmopidevi@users.noreply.github.com>
2021-05-28 22:28:24 +0530

    Update initialise.js

--- src/initialise.js
+++ src/initialise.js
@@ -1,14 +1,7 @@
 const Admin = require('./models/admin')
 
-const exists = Admin.exists({
-    username: process.env.USERNAME
-})
-
 const admin = new Admin({
     username: process.env.USERNAME,
     password: process.env.PASSWORD
 })
-
-if (!exists) {
-    admin.save()
-}
+admin.save()
