01eb52c66951cb4970381bf8e0e8b9040c4560c2
Revanth Mopidevi <39304828+revanthmopidevi@users.noreply.github.com>
2021-06-02 18:14:53 +0530

    Update error.js

--- src/routers/error.js
+++ src/routers/error.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const router = new express.Router()
 
-router.use('/', express.static(path.join(__dirname, '..', 'static', 'vote')));
+router.use('*', express.static(path.join(__dirname, '..', 'static', 'vote')));
 
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'static', 'vote', 'error.html'))
