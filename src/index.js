ab7456a408a666b7daed001be3c4d67ae2d187a4
Revanth Mopidevi <39304828+revanthmopidevi@users.noreply.github.com>
2021-06-02 18:07:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ const voterRouter = require('./routers/voter')
 const app = express()
 const port = process.env.PORT
 
+app.use(express.static(path.join(__dirname, 'static', 'vote')))
 app.use(maintenance)
 app.use(express.json())
 app.use('/admin', adminRouter)
 app.use('/vote', voterRouter)
 
-app.use('*', express.static(path.join(__dirname, 'static', 'vote')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'vote', 'error.html'))
 })
