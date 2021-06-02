dd5654a43541cdb4e34ece3dedb3cd6717442bee
Revanth Mopidevi <39304828+revanthmopidevi@users.noreply.github.com>
2021-06-02 18:11:22 +0530

    Create error.js

---
+++ src/routers/error.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const router = new express.Router()
+
+router.use('/', express.static(path.join(__dirname, '..', 'static', 'vote')));
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'static', 'vote', 'error.html'))
+})
+
+module.exports = router
